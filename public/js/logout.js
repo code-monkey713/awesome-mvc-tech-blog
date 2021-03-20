ca7ab8e5f51c022507c1b1bdee78cf9e41bb5b92
Tony Tran <codemonkey713@gmail.com>
2021-03-20 00:12:20 -0500

    added CSS and JS files for the view to function with the backend

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,14 +1,12 @@
-const logout = async () => {
-  const response = await fetch('/api/users/logout', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
+async function logoutHandler() {
+  const response = await fetch("/api/users/logout", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
   });
 
   if (response.ok) {
-    document.location.replace('/');
-  } else {
-    alert(response.statusText);
+    document.location.replace("/home");
   }
-};
+}
 
-document.querySelector('#logout').addEventListener('click', logout);
+document.querySelector("#logoutBtn").addEventListener("click", logoutHandler);
