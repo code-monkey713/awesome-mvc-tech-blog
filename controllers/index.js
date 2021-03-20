75d6eb74bcae612a69c27e078f50847e29690493
Tony Tran <codemonkey713@gmail.com>
2021-03-20 01:22:02 -0500

    fixed the authorization helper and now the application is working

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,9 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
-// const dashboardRoutes = require('./dashboardRoutes');
+const apiRoutes = require("./api");
+const homeRoutes = require("./home-routes");
 
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
-// router.use('/dashboard', dashboardRoutes);
+router.use("/", homeRoutes);
+router.use("/api", apiRoutes);
 
 module.exports = router;
